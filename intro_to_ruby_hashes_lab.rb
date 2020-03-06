88515ef182f353528433e4cf26f828cb69b15c24
Anthony mendoza <anthonymendoza9922@gmail.com>
2020-03-06 01:08:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,11 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-if hash[key] +=1
-else hash[key]+=1
+if hash[key]
+  hash[key]+=1
+  
+else hash[key]
+  hash[key]+=1
+  
 end
 end
